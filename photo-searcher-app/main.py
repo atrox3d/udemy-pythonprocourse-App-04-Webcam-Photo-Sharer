6ad2a7794e5afd0e92e7d0ba8709a9bf2acfd2ba
atrox3d <atrox3d@gmail.com>
2022-09-04 09:33:23 +0200

    77. The Four Main Objects of Kivy

---
+++ photo-searcher-app/main.py
@@ -0,0 +1,39 @@
+# The four main objects of Kivy
+#
+#                     +------------------+
+#                     |       App        |
+#                     +------------------+
+#
+#                     +------------------+
+#                     |   ScreenManager  |
+#                     +------------------+
+#
+#                     +------------------+
+#                     |      Screen      |
+#                     +------------------+
+#
+#                     +------------------+
+#                     |      Widget      |
+#                     +------------------+
+#
+################################################################################
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class FirstScreen(Screen):
+    def search_image(self):
+        pass
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+
+MainApp().run()
