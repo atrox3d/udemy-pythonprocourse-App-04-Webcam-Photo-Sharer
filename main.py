c08a8f3e54eff046e597dbdfbf0f1c30471a6efe
atrox3d <atrox3d@gmail.com>
2022-09-05 15:44:19 +0200

    switch screen

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ class CameraScreen(Screen):
         current_time = time.strftime('%Y%m%d-%H%M%S')
         filepath = f'files/{current_time}.png'
         self.ids.camera.export_to_png(filepath)
+        self.manager.current = 'image_screen'
 
 
 class ImageScreen(Screen):
